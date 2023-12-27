e4bbff7e46ce5efba208eaebcff65482ec213bcc
lauti3314 <lautaro3314@hotmail.com>
2023-12-27 13:11:59 -0300

    nuevas actualizaciones

---
+++ src/data/constants.js
@@ -0,0 +1,19 @@
+export const Bio = {
+    name: "Lautaro Tejada",
+    roles: [
+        "Full Stack Developer",
+        "Programmer",
+    ],
+    description: 
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/lauti3314",
+    CV: "",
+    linkedin:"",
+    twitter: "",
+    instagram: "",
+
+};
+
+export const skils = {
+    
+}
